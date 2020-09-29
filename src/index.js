b38df9276029e0c3f24f73c73a119d5c4c28a4b3
Kimi-Toshiko <kimitoshiko@gmail.com>
2020-09-29 18:12:46 +0500

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-
 exports.min = function min (array) {
-  return 0;
+  return ((array === undefined) || (array.length === 0)) ? 0 : Math.min.apply(0, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return ((array === undefined) || (array.length === 0)) ? 0 : Math.max.apply(0, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ((array === undefined) || (array.length === 0))
+    return 0;
+  else {
+    let hopper = 0;
+    for (let i = 0; i < array.length; i++) {
+      hopper += array[i];
+    }
+    return hopper / array.length;
+  }
 }
